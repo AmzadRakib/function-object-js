c87f4509ab4186ed8312b6209e83d59b58d306e6
AmzadRakibe <amzadhrakib570@gmail.comm>
2023-02-28 05:25:50 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,11 @@
+// Function declaration 
+
+function startFan() {
+    console.log('Stund Up');
+    console.log('walk and towards the switch');
+    console.log('press the switch');
+}
+
+// call the function 
+
+startFan();
