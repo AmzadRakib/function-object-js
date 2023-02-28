9a0bfc0cd003b183a72606a9dd2c2e4c07a0f291
AmzadRakibe <amzadhrakib570@gmail.comm>
2023-02-28 23:32:24 +0600

    resolved

--- properties.js
+++ properties.js
@@ -8,37 +8,37 @@ var shopingCard = {
 // dynamic way to get properties value 
 // way number 1 get value 
 var penCount = shopingCard.pen;
-/* console.log(penCount); */
+console.log(penCount);
 
 // Way Number 2  get value
 var penCount2 = shopingCard['pen'];
-/* console.log(penCount2); */
+console.log(penCount2);
 
 // way Number 3 to get properties
 var propertyName = "mouse"
 var penCount3 = shopingCard[propertyName];
-/* console.log(propertyName, penCount3); */
+console.log(propertyName, penCount3);
 
 // get only object properties (keys) Name 
 
 var properties = Object.keys(shopingCard);
-/* console.log(properties); */
+console.log(properties);
 
 // get only object properties (keys) Value 
 var propertiesVlue = Object.values(shopingCard);
-/* console.log(propertiesVlue); */
+console.log(propertiesVlue);
 
 // dynamic way to set properties value
 
 // way Number 1 
 shopingCard.mouse = 10;
-/* console.log(shopingCard.mouse); */
+console.log(shopingCard.mouse);
 
 // way number 2 set property value
 shopingCard['pen'] = 13;
-/* console.log(shopingCard['pen']); */
+console.log(shopingCard['pen']);
 
 // way number 3 set propety value 
 
 shopingCard[propertyName] = 15
-/* console.log(shopingCard.mouse) */
+console.log(shopingCard.mouse)
